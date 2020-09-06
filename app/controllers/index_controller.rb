5499407aa41365c2376e00fd2818fa9f23ff3e24
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-09-06 22:30:27 +0000

    Automatically backed up by Learn

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,7 +2,7 @@ class IndexController < ApplicationController
   
   def students 
     @students = Student.all
-    render "app/views/students/index"
+    render "/views/students/index"
   end
   
   
